9db9209ce0d2938f84f73910445fd60a62f17995
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-04-02 18:21:46 +0300

    Убраны неиспользующиеся импорты

--- src/main/java/ru/tolstikhin/controller/SQLController.java
+++ src/main/java/ru/tolstikhin/controller/SQLController.java
@@ -2,15 +2,12 @@ package ru.tolstikhin.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.tolstikhin.servlet.LoginServlet;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class SQLController {
